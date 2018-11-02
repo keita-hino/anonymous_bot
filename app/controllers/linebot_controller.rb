eb082e3f0348f212d9406a4dd746e1b619e6b970
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-11-02 17:47:03 +0900

    feat:単純な返答

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -6,10 +6,11 @@ class LinebotController < ApplicationController
 
  def client
    @client ||= Line::Bot::Client.new { |config|
-     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+     config.channel_secret = ENV["ANONYMOUS_LINE_CHANNEL_SECRET"]
+     config.channel_token = ENV["ANONYMOUS_LINE_CHANNEL_TOKEN"]
    }
  end
+
 
  def callback
    body = request.body.read
@@ -26,13 +27,11 @@ class LinebotController < ApplicationController
      when Line::Bot::Event::Message
        case event.type
        when Line::Bot::Event::MessageType::Text
-         case event.message['text']
-           message = {
-             type: "text",
-             text: event.message['text']
-           }
-           client.reply_message(event['replyToken'], message)
-         end
+         message = {
+           type: "text",
+           text: event.message['text']
+         }
+         client.reply_message(event['replyToken'], message)
        end
      end
    }
